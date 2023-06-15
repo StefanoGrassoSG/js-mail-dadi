ebb38f80615a1f2a9241145c6e2bd2ac774c2e50
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-15 17:19:17 +0200

    remove cycle in js

--- mail/js/script.js
+++ mail/js/script.js
@@ -11,25 +11,26 @@ const mailList = [
     'pietro@gmail.com'
 ]
 
-for (let j = 0; j < mailList.length; j++) {
+
    
+form.addEventListener('submit', function(evt){
+    evt.preventDefault();
+    let input = document.getElementById("email").value;
+    console.log(input)
 
-    form.addEventListener('submit', function(evt){
-        evt.preventDefault();
-        let input = document.getElementById("email").value;
-        console.log(input)
-
-        if (input == mailList[j]) {
-            alert("Hai effettutato l'accesso")
-        }
+    if(mailList.indexOf(input) !== -1) {
+        alert("Hai effettutato l'accesso")
+    }
     
-        else  {
-            alert("Questa email non ha accesso")
-        }
+    else {
+        alert("Questa email non ha accesso")
+    }
         
-    })
+})
 
     
 
-}
+
+
+
 
