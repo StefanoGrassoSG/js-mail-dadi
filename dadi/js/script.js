ee781d4c6bf09a4fe1a925a578ea6886feca5b1e
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-15 15:24:56 +0200

    add button in html/css and conditions in js

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -2,3 +2,19 @@
 
 const number = Math.floor(Math.random() * 6) + 1;
 
+const anotherNumber = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('.red').append(number);
+document.querySelector('.yellow').append(anotherNumber);
+
+if (number > anotherNumber) {
+    console.log("ROSSO hai vinto");
+}
+
+else if (number == anotherNumber) {
+    console.log("PARI")
+}
+
+else {
+    console.log("YELLOW hai vinto")
+}
